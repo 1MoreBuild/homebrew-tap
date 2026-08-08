8f0f6afcf28cfa804b0b78ad728a033850bc764b
liuhaitian <liuhaitian@bytedance.com>
2026-08-08 00:53:53 -0700

    chore: bump Lane to v0.1.3

--- Casks/lane.rb
+++ Casks/lane.rb
@@ -1,9 +1,9 @@
 cask "lane" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.1"
-  sha256 arm:   "6c8bc70e6026083433e1dcddc76da834f6301ac5a51e465eadebfec45c5771f3",
-         intel: "9753852f7fd6be6d599275c4e0f7968f8edb5c6123dd2dfbeb5220f736a7cdfe"
+  version "0.1.3"
+  sha256 arm:   "4812183cad3b34759d88ad049450beea4e3c7d093e98a98f448ca4abc22ea6c1",
+         intel: "2bb5b8151f1b392b984b78adb46a9c5eebf35bef6efcef5cfcf67b17f6d3243d"
 
   url "https://github.com/1MoreBuild/Lane/releases/download/v#{version}/Lane-#{version}-mac-#{arch}.dmg",
       verified: "github.com/1MoreBuild/Lane/"
